bd622ac688f3ec1e0fb1ac3b70300763a1ae38d0
siilao <97461255@qq.com>
2025-03-06 20:13:30 +0800

    message

--- silao.sh
+++ silao.sh
@@ -11,6 +11,90 @@ gl_bai='\033[0m'
 gl_zi='\033[35m'
 gl_slao='\033[96m'
 
+linux_ps() {
+
+	clear
+	send_stats "系统信息查询"
+
+	ip_address
+
+	cpu_info=$(lscpu | awk -F': +' '/Model name:/ {print $2; exit}')
+
+	cpu_usage_percent=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else printf "%.0f\n", (($2+$4-u1) * 100 / (t-t1))}' \
+		<(grep 'cpu ' /proc/stat) <(sleep 1; grep 'cpu ' /proc/stat))
+
+	cpu_cores=$(nproc)
+
+	mem_info=$(free -b | awk 'NR==2{printf "%.2f/%.2f MB (%.2f%%)", $3/1024/1024, $2/1024/1024, $3*100/$2}')
+
+	disk_info=$(df -h | awk '$NF=="/"{printf "%s/%s (%s)", $3, $2, $5}')
+
+	ipinfo=$(curl -s ipinfo.io)
+	country=$(echo "$ipinfo" | grep 'country' | awk -F': ' '{print $2}' | tr -d '",')
+	city=$(echo "$ipinfo" | grep 'city' | awk -F': ' '{print $2}' | tr -d '",')
+	isp_info=$(echo "$ipinfo" | grep 'org' | awk -F': ' '{print $2}' | tr -d '",')
+
+
+	cpu_arch=$(uname -m)
+
+	hostname=$(uname -n)
+
+	kernel_version=$(uname -r)
+
+	congestion_algorithm=$(sysctl -n net.ipv4.tcp_congestion_control)
+	queue_algorithm=$(sysctl -n net.core.default_qdisc)
+
+	# 尝试使用 lsb_release 获取系统信息
+	os_info=$(grep PRETTY_NAME /etc/os-release | cut -d '=' -f2 | tr -d '"')
+
+	output_status
+
+	current_time=$(date "+%Y-%m-%d %I:%M %p")
+
+
+	swap_info=$(free -m | awk 'NR==3{used=$3; total=$2; if (total == 0) {percentage=0} else {percentage=used*100/total}; printf "%dMB/%dMB (%d%%)", used, total, percentage}')
+
+	runtime=$(cat /proc/uptime | awk -F. '{run_days=int($1 / 86400);run_hours=int(($1 % 86400) / 3600);run_minutes=int(($1 % 3600) / 60); if (run_days > 0) printf("%d天 ", run_days); if (run_hours > 0) printf("%d时 ", run_hours); printf("%d分\n", run_minutes)}')
+
+	timezone=$(current_timezone)
+
+
+	echo ""
+	echo -e "系统信息查询"
+	echo -e "${gl_slao}------------------------"
+	echo -e "${gl_slao}主机名: ${gl_bai}$hostname"
+	echo -e "${gl_slao}运营商: ${gl_bai}$isp_info"
+	echo -e "${gl_slao}------------------------"
+	echo -e "${gl_slao}系统版本: ${gl_bai}$os_info"
+	echo -e "${gl_slao}Linux版本: ${gl_bai}$kernel_version"
+	echo -e "${gl_slao}------------------------"
+	echo -e "${gl_slao}CPU架构: ${gl_bai}$cpu_arch"
+	echo -e "${gl_slao}CPU型号: ${gl_bai}$cpu_info"
+	echo -e "${gl_slao}CPU核心数: ${gl_bai}$cpu_cores"
+	echo -e "${gl_slao}------------------------"
+	echo -e "${gl_slao}CPU占用: ${gl_bai}$cpu_usage_percent%"
+	echo -e "${gl_slao}物理内存: ${gl_bai}$mem_info"
+	echo -e "${gl_slao}虚拟内存: ${gl_bai}$swap_info"
+	echo -e "${gl_slao}硬盘占用: ${gl_bai}$disk_info"
+	echo -e "${gl_slao}------------------------"
+	echo -e "${gl_slao}$output"
+	echo -e "${gl_slao}------------------------"
+	echo -e "${gl_slao}网络拥堵算法: ${gl_bai}$congestion_algorithm $queue_algorithm"
+	echo -e "${gl_slao}------------------------"
+	echo -e "${gl_slao}公网IPv4地址: ${gl_bai}$ipv4_address"
+	echo -e "${gl_slao}公网IPv6地址: ${gl_bai}$ipv6_address"
+	echo -e "${gl_slao}------------------------"
+	echo -e "${gl_slao}地理位置: ${gl_bai}$country $city"
+	echo -e "${gl_slao}系统时区: ${gl_bai}$timezone"
+	echo -e "${gl_slao}系统时间: ${gl_bai}$current_time"
+	echo -e "${gl_slao}------------------------"
+	echo -e "${gl_slao}系统运行时长: ${gl_bai}$runtime"
+	echo
+
+
+
+}
+
 silao_update() {
 
 	send_stats "脚本更新"
@@ -75,7 +159,7 @@ echo -e "-输入${gl_huang}s${gl_slao}可快速启动此脚本${gl_bai}"
 echo -e "${gl_slao}------------------------${gl_bai}"
 echo -e "${gl_slao}1.   ${gl_bai}系统信息查询"
 echo -e "${gl_slao}2.   ${gl_bai}系统更新"
-echo -e "${gl_slao}3.   ${gl_bai}系统sdfsd清理"
+echo -e "${gl_slao}3.   ${gl_bai}系统清理"
 echo -e "${gl_slao}------------------------${gl_bai}"
 echo -e "${gl_slao}00.  ${gl_bai}脚本更新"
 echo -e "${gl_slao}------------------------${gl_bai}"
